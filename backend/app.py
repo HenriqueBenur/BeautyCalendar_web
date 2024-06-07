c05633f6be11f5b8ab4d0f79cbcdda4071c2ca23
HenriqueBenur <henriquebenurcif@gmail.com>
2024-06-07 17:13:47 -0300

    Backend First Commit

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,9 @@
-# app.py
 from flask import Flask
 from flask_cors import CORS
 from routes import main
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
 app.register_blueprint(main)
 
 if __name__ == '__main__':
